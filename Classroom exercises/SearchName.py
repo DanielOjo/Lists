f9c18809d0cb37c292146fd762734500991a5647
DanielOjo <danielojo@live.com>
2014-12-15 12:16:16 +0000

    update

--- Classroom exercises/SearchName.py
+++ Classroom exercises/SearchName.py
@@ -5,13 +5,17 @@
 NamesList = ['Jim', 'Bob', 'Alison', 'Jo', 'James']
 
 ElementSought = input('Enter the name you are searching for : ')
+NamesList.append(ElementSought)
 ThisElement = 0
+
+#If you enter a name that is not in the list of names them the This Element will look try to assign to a Element in the list that isnt there
 Found = False
 while not Found:
        if NamesList[ThisElement] == ElementSought:
           Found = True
        else:
           ThisElement = ThisElement + 1
+          
 
 if Found:
    print('{0} was in element {1} in the list'.format(ElementSought, ThisElement))
