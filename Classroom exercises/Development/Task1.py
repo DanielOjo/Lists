69ab478ff0c5d1b5925c557249d25c92047d41b6
DanielOjo <danielojo@live.com>
2015-01-08 23:59:30 +0000

    Done

--- Classroom exercises/Development/Task1.py
+++ Classroom exercises/Development/Task1.py
@@ -2,24 +2,39 @@
 #Lists Development Exercise Task 1 
 #05/01/15
 
-import random
+def random_number():
+    import random 
+    random_num = random.randint(1,11)
+    return random_num
+ 
+def countrys():
+    country_list = ["England","US","France","Spain","Belgium","Japan","China","Italy","Germany","Canada"]
+    return country_list
+def capitals():
+    capital_list =  ["London","WashingtonDC", "Madrid", "Paris","Brussels","Tokyo","Hongkong","Rome","Berlin","Ottawa"]
+    return capital_list
 
-country_list = ["England","US","France","Spain","Belgium","Japan","China","Italy","Germany","Canada"]
-capital_list = ["London","WashingtonDC","Paris","Brussels","Tokyo","Hongkong","Rome","Berlin","Ottawa"]
+def process_country(country_list,random_num):
+    country = country_list[random_num]
+    return country
+def process_capital(capital_list,random_num):
+    capital = capital_list[random_num]
+    return capital
 
-question = random.choice(country_list)
-print(question)
-correct_answer = capital_list(int(question) 
+def ask_user(country,capital):
+    user_answer = input("Name the capital of {0}: ".format(country))
+    if user_answer == capital:
+        print("That is correct")
+    else:
+        print("That is wrong")
 
-user_answer = input("Please type the capital city of this country: ")
+def main_program():
+    random_num = random_number()
+    country_list = countrys()
+    capital_list = capitals()
+    country = process_country(country_list,random_num)
+    capital = process_capital(capital_list,random_num)
+    ask_user(country,capital)
 
-answer = False
-while not answer:
-       if correct_answer == user_answer:
-          Found = True
-       else:
-          print("Your answer was incorrect")
-
-if answer:
-    print("Your answer was correct")
-
+#main program
+main_program()
